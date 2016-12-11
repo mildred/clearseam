ab871934c7da025513b17b357c6db9653beff9ad
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2016-12-11 13:43:01 +0100

    Implement CSS selectors and object rendering to vdom

--- templateless.js
+++ templateless.js
@@ -1,14 +1,14 @@
 function $templateless(domlib, debug){
-  var console = window.console
+  var dconsole = window.console
   if(!debug) {
-    console = {
+    dconsole = {
       log: function(){},
       group: function(){},
       groupEnd: function(){},
     }
   }
   return compile
-  function compile(domtmpl, directives0, matches_optional){
+  function compile(domtmpl, directives0, matches_optional, parentdom){
     var dirstack = normdirectives(directives0)
 
     var children = domlib.children(domtmpl)
@@ -20,7 +20,7 @@ function $templateless(domlib, debug){
     var cl = children.length
     var merged_directives = {}
 
-    console.log("Compile %s %o (%d matches): %o", domlib.tagName(domtmpl), domtmpl, matches.length,
+    dconsole.log("Compile %s %o (%d matches): %o", domlib.tagName(domtmpl), domtmpl, matches.length,
         dirstack.length == 1 ? dirstack[0] : dirstack)
 
     while(dirstack.length > 0){
@@ -29,7 +29,7 @@ function $templateless(domlib, debug){
       for(var tsel in directives){
         var dsel = directives[tsel]
         var match
-        if(match = tmatch(domtmpl, tsel, dsel)) {
+        if(match = tmatch(parentdom, domtmpl, tsel, dsel)) {
           if(match.d) dirstack.push(match.d)
           if(match.f) matches.push(match.f)
           delete directives[tsel]
@@ -38,7 +38,7 @@ function $templateless(domlib, debug){
       merged_directives = merge([merged_directives, directives])
     }
 
-    if(cl > 0) console.group("Match "+cl+" children:")
+    if(cl > 0) dconsole.group("Match "+cl+" children:")
     for(var tsel in merged_directives){
       var dsel = merged_directives[tsel]
       var match
@@ -46,50 +46,50 @@ function $templateless(domlib, debug){
         cmatches[i] = cmatches[i] || []
         cdirs[i] = cdirs[i] || clone(merged_directives)
         cdirs2[i] = cdirs2[i] || []
-        if(match = tmatch(children[i], tsel, dsel)) {
+        if(match = tmatch(domtmpl, children[i], tsel, dsel)) {
           if(match.d) cdirs2[i].push(match.d)
           if(match.f) cmatches[i].push(match.f)
           delete cdirs[i][tsel]
         }
       }
     }
-    if(cl > 0) console.groupEnd()
+    if(cl > 0) dconsole.groupEnd()
 
-    if(cl > 0) console.group("Compile "+cl+" children:")
+    if(cl > 0) dconsole.group("Compile "+cl+" children:")
     for(var i = 0; i < cl; i++){
       var subdirectives = cdirs2[i] && cdirs2[i].length ? cdirs2[i] : cdirs[i]
-      childrentmpl[i] = compile(children[i], subdirectives, cmatches[i])
+      childrentmpl[i] = compile(children[i], subdirectives, cmatches[i], domtmpl)
     }
-    if(cl > 0) console.groupEnd()
+    if(cl > 0) dconsole.groupEnd()
 
     return function(data){
-      console.log("Render %s %o using %o", domlib.tagName(domtmpl), domtmpl, data)
+      dconsole.log("Render %s %o using %o", domlib.tagName(domtmpl), domtmpl, data)
       var dom = domlib.clone(domtmpl, false)
       var ml = matches.length
-      if(cl) console.group("render " + cl + " children")
+      if(cl) dconsole.group("render " + cl + " children")
       for(var i = 0; i < cl; i++){
         domlib.addChild(dom, childrentmpl[i](data))
       }
-      if(cl) console.groupEnd()
-      if(ml>1) console.group("render " + ml + " matches")
+      if(cl) dconsole.groupEnd()
+      if(ml>1) dconsole.group("render " + ml + " matches")
       var loop = [{dom: dom, data: data}]
       for(var i = 0; i < ml; i++){
         var match = matches[i]
         var newloop = []
         var ll = loop.length
-        if(ll>1) console.group("render " + i + ".* " + ll + " loop items")
+        if(ll>1) dconsole.group("render " + i + ".* " + ll + " loop items")
         for(var j = 0; j < ll; j++) {
           var item = loop[j]
-          console.log("render %d.%d, %o with %o", i, j, item.dom, item.data)
+          dconsole.log("render %d.%d, %o with %o", i, j, item.dom, item.data)
           var newitems = match(item.dom, item.data)
-          if (newitems) console.log("render %d.%d to %o", i, j, newitems)
+          if (newitems) dconsole.log("render %d.%d to %o", i, j, newitems)
           else newitems = [item]
           newloop = newloop.concat(newitems)
         }
-        if(ll>1) console.groupEnd()
+        if(ll>1) dconsole.groupEnd()
         loop = newloop
       }
-      if(ml>1) console.groupEnd()
+      if(ml>1) dconsole.groupEnd()
       if(loop.length == 1) {
         return loop[0].dom
       } else {
@@ -128,33 +128,25 @@ function $templateless(domlib, debug){
   //
   // dsel: {DIRECTIVES}
   //   TODO
-  function tmatch(domtmpl, tsel, dsel){
-    var m = tsel.match(/^(\+)?([^\@\&\+]+)?(\@([^\+]+)|\&([^\+]+))?(\+)?$/)
-    if(!m) return null;
-    var prepend = m[1];
-    var selector = m[2].toLowerCase();
-    var suffix = m[3];
-    var attr = m[4];
-    var event = m[5];
-    var append = m[6];
-
-    var matchSelector = domlib.tagName(domtmpl) && (
-        (selector === ".") ||
-        (selector === "" && suffix !== "") ||
-        (selector === domlib.tagName(domtmpl).toLowerCase()))
-    if(!matchSelector) {
-      //console.log({tmatch: domtmpl, tsel: tsel, dsel: dsel, result: false})
-      return null;
+  function tmatch(parentdom, domtmpl, tsel, dsel){
+    var s = matchtsel(tsel)
+    if(!s.match(domtmpl, parentdom)){
+      return null
     }
 
     var loopinst
     var loopdir
+    var loopvar
+    var loopdsel
     if(typeof(dsel) === 'object') {
-      if(event !== undefined && event !== "") {
+      if(s.event) {
         error("Cannot loop over an event")
       }
-      if(attr !== undefined && attr !== "") {
+      if(s.attr) {
         error("Cannot loop over an attribute")
+      }
+      if(s.prop) {
+        error("Cannot loop over a property")
       }
       for(dir in dsel) {
         var m = dir.match( /^(\w+)\s*<-\s*(\S+)?$/ )
@@ -162,14 +154,14 @@ function $templateless(domlib, debug){
           if(loopinst) error("Loop directive must contain only one loop instruction")
           loopinst = m
           loopdir = dsel[dir]
+          loopvar = m[1]
+          loopdsel = m[2]
         }
       }
     }
 
     if(loopinst) {
-      console.log("Match %s to loop %s: %o", tsel, loopinst[0], loopdir)
-      var loopvar = m[1]
-      var loopdsel = m[2]
+      dconsole.log("Match %s to loop %s: %o", tsel, loopinst[0], loopdir)
       var loopdataf = dataselectfn(loopdsel, false)
       return {
         d: loopdir,
@@ -177,7 +169,7 @@ function $templateless(domlib, debug){
           var collection = loopdataf.call(data, {context: data})
           var cl = collection.length
           var loop = []
-          console.log("render list of %d: %o", cl, collection)
+          dconsole.log("render list of %d: %o", cl, collection)
           for(var i = 0; i < cl; i++) {
             var element = domlib.clone(dom)
             var elemdata = Object.create(data, {})
@@ -189,23 +181,18 @@ function $templateless(domlib, debug){
       }
     }
 
-    var dataf = dataselectfn(dsel, event !== undefined && event !== "")
+    var dataf = dataselectfn(dsel, s.event)
 
-    if(suffix === "" || suffix === undefined) {
-      //console.log({tmatch: domtmpl, tsel: tsel, dsel: dsel, result: true})
-      console.log("Match %s to tag: %o", tsel, dsel)
-      if(prepend && append) {
-        error("Cannot append and insert before")
-      }
+    if(s.content) {
+      //dconsole.log({tmatch: domtmpl, tsel: tsel, dsel: dsel, result: true})
+      dconsole.log("Match %s to tag: %o", tsel, dsel)
       return {f: function(dom, data){
         var newcontent = dataf.call(data, {context: data})
-        console.log("render tag %o: %o", dom, newcontent)
-        if(typeof(newcontent) == 'string') {
-          newcontent = domlib.text(newcontent)
-        }
-        if(prepend) {
+        dconsole.log("render tag %o: %o", dom, newcontent)
+        newcontent = domlib.toVDOM(newcontent)
+        if(s.insert) {
           domlib.insertChild(dom, newcontent)
-        } else if(append) {
+        } else if(s.append) {
           domlib.addChild(dom, newcontent)
         } else {
           domlib.clearChildren(dom)
@@ -214,25 +201,25 @@ function $templateless(domlib, debug){
         return null
       }}
     } else {
-      //console.log({tmatch: domtmpl, tsel: tsel, dsel: dsel, result: true})
-      if(event) console.log("Match %s to event %s: %o", tsel, event, dsel)
-      if(attr) console.log("Match %s to attr %s: %o", tsel, attr, dsel)
-      if(event && (prepend || append)) {
+      //dconsole.log({tmatch: domtmpl, tsel: tsel, dsel: dsel, result: true})
+      if(s.event) dconsole.log("Match %s to event %s: %o", tsel, s.event, dsel)
+      if(s.attr) dconsole.log("Match %s to attr %s: %o", tsel, s.attr, dsel)
+      if(s.event && (s.insert || s.append)) {
         error("Cannot insert before or append to and event")
       }
       return {f: function(dom, data){
         var userdata = dataf.call(data, {context: data})
-        if(event) {
-          console.log("render event %s: %o", event, userdata)
-          domlib.addEventHandler(dom, event, userdata)
+        if(s.event) {
+          dconsole.log("render event %s: %o", s.event, userdata)
+          domlib.addEventHandler(dom, s.event, userdata)
         } else {
-          console.log("render attribute %s: %o", attr, userdata)
-          if(prepend) {
-            domlib.setAttr(dom, attr, userdata + domlib.attr(dom, attr))
-          } else if(append) {
-            domlib.setAttr(dom, attr, domlib.attr(dom, attr) + userdata)
+          dconsole.log("render attribute %s: %o", s.attr, userdata)
+          if(s.insert) {
+            domlib.setAttr(dom, s.attr, userdata + domlib.attr(dom, s.attr))
+          } else if(s.append) {
+            domlib.setAttr(dom, s.attr, domlib.attr(dom, s.attr) + userdata)
           } else {
-            domlib.setAttr(dom, attr, userdata)
+            domlib.setAttr(dom, s.attr, userdata)
           }
         }
         return null
@@ -240,6 +227,163 @@ function $templateless(domlib, debug){
     }
 
     return null;
+  }
+
+  function matchtsel(tsel){
+    var sel = {}
+    tsel = matchappend(sel, tsel)
+    tsel = matchattr(sel, tsel)
+    sel.criterias = []
+    while(tsel != "") {
+      tsel = matchcriterias(sel, tsel)
+    }
+    sel.match = function(dom, parentdom){
+      var cl = sel.criterias.length
+      for(var i = 0; i < cl; i++) {
+        if(!sel.criterias[i](dom, parentdom)) return false
+      }
+      return true
+    }
+    return sel
+  }
+
+  function matchappend(sel, tsel){
+    sel.insert = false
+    sel.append = false
+    var m = tsel.match(/^(\+)?\s*(.*?)\s*(\+)?$/)
+    if(!m) return tsel
+    sel.insert = m[1] !== "" && m[1] !== undefined
+    sel.append = m[3] !== "" && m[3] !== undefined
+    return m[2]
+  }
+
+  function matchattr(sel, tsel){
+    sel.attr = null
+    sel.event = null
+    sel.prop = null
+    sel.content = false
+
+    var m = tsel.match(/^(.*)\@(.+)$/)
+    if(m) {
+      sel.attr = m[2]
+      return m[1]
+    }
+
+    var m = tsel.match(/^(.*)\&(.+)$/)
+    if(m) {
+      sel.event = m[2]
+      return m[1]
+    }
+
+    var m = tsel.match(/^(.*)\$(.+)$/)
+    if(m) {
+      sel.prop = m[2]
+      return m[1]
+    }
+
+    sel.content = true
+    return tsel
+  }
+
+  function matchcriterias(sel, tsel) {
+    if(tsel == "*" || tsel == "."){
+      sel.criterias.push(function(){
+        return true
+      })
+      return ""
+    }
+
+    var m = tsel.match(/^(.*)\:not\(([^\(\)]+(\([^\(\)]*\))?)\)$/)
+    if(m) {
+      var subsel = {
+        criterias: []
+      }
+      if(matchcriterias(subsel, m[2]) == ""){
+        sel.criterias.push(function(dom, parent){
+          return !subsel.criterias[0](dom, parent)
+        })
+        return m[1]
+      }
+    }
+
+    function crit_index(index, dom, parent){
+      var children = domlib.children(parent)
+      if(index < 0) {
+        return children[children.length+index] === dom
+      } else {
+        return children[1+index] === dom
+      }
+    }
+
+    function crit_matchattr(attr, op, value, dom){
+      var actual = domlib.attr(dom, attr)
+      if(actual === undefined) return false;
+      if(op == "=") return value === actual;
+      if(op == "~=") return actual.split(" ").indexOf(value) != -1
+      if(op == "|=") return actual.split("-").indexOf(value) == 0
+      if(op == "^=") return actual.startsWith(value)
+      if(op == "*=") return actual.includes(value)
+      error("Unknown operator " + op)
+      return false
+    }
+
+    var m = tsel.match(/^(.*)\.([^\:\#\.\[\]]+)$/)
+    if(m) {
+      sel.criterias.push(crit_matchattr.bind(this, 'class', '~=', m[2]))
+      return m[1]
+    }
+
+    var m = tsel.match(/^(.*)\#([^\:\#\.\[\]]+)$/)
+    if(m) {
+      sel.criterias.push(crit_matchattr.bind(this, 'id', '=', m[2]))
+      return m[1]
+    }
+
+    var m = tsel.match(/^(.*)\[(-?[0-9]+)\]$/)
+    if(m) {
+      sel.criterias.push(crit_index.bind(this, parseInt(m[2])))
+      return m[1]
+    }
+
+    var m = tsel.match(/^(.*)\:first-child$/)
+    if(m) {
+      sel.criterias.push(crit_index.bind(this, 1))
+      return m[1]
+    }
+
+    var m = tsel.match(/^(.*)\:last-child$/)
+    if(m) {
+      sel.criterias.push(crit_index.bind(this, -1))
+      return m[1]
+    }
+
+    var m = tsel.match(/^(.*)\:nth(-last)?-child\((-?[0-9]+)\)$/)
+    if(m) {
+      sel.criterias.push(crit_index.bind(this, m[2] ? -parseInt(m[3]) : parseInt(m[3])))
+      return m[1]
+    }
+
+    var m = tsel.match(/^(.*)\[([^\[\]]+)([\~\|\^\$\*]?=)([^\[\]]+)\]$/)
+    if(m) {
+      sel.criterias.push(crit_matchattr.bind(this, m[2], m[3], m[4]))
+      return m[1]
+    }
+
+    var m = tsel.match(/^(.*)\[([^\[\]]+)\]$/)
+    if(m) {
+      var attr = m[2]
+      sel.criterias.push(function(dom){
+        return domlib.attr(dom, attr) !== undefined
+      })
+      return m[1]
+    }
+
+    sel.criterias.push(function(dom){
+      dconsole.log(domlib)
+      var tagName = domlib.tagName(dom)
+      return tagName !== undefined && tagName.toLowerCase() == tsel.toLowerCase()
+    })
+    return ""
   }
 
   // from pure.js
@@ -376,6 +520,26 @@ $templateless.citojsdom = (function(){
     if(typeof(domnode) != 'object') return [domnode]
     if(!domnode.tag) return citojsdom.children(domnode)
     return [domnode]
+  }
+
+  citojsdom.isVDOM = function(domnode){
+    return domnode.tag !== undefined || domnode.children !== undefined
+  }
+
+  citojsdom.toVDOM = function(domnode){
+    if(typeof(domnode) == 'string') {
+      return {tag: '#', children: domnode}
+    } else if(citojsdom.isVDOM(domnode)) {
+      return domnode
+    } else if(typeof domnode.template === "function") {
+      return domnode.template()
+    } else if(typeof domnode.render === "function") {
+      return domnode.render()
+    } else if(typeof domnode.vdom === "function") {
+      return domnode.vdom()
+    } else {
+      return domnode
+    }
   }
 
   citojsdom.clone = function dom_clone(domnode, recursive){
